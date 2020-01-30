61891165c997259f64b983c82a22fa10108f2869
Leo Jensen <lion0005@zbc.dk>
2020-01-30 12:43:11 +0100

    First Commit

--- Robo_Robi/app/src/main/java/com/example/robo_robi/MainActivity.java
+++ Robo_Robi/app/src/main/java/com/example/robo_robi/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.robo_robi;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.example.robo_robi.ui.home.HomeViewModel;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +17,48 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    public BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onReceive: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "mBroadcastReceiver1: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "mBroadcastReceiver1: STATE ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "mBroadcastReceiver1: STATE TURNING ON");
+                        break;
+                }
+            }
+        }
+    };
+
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: Called");
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReceiver);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +74,44 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
+
+
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
     }
+
+
+
+    public void enableDisableBT(){
+        if(mBluetoothAdapter == null){
+            Log.d(TAG, "enableDisableBT: Does not have BT capabilities.");
+        }
+        else if(!mBluetoothAdapter.isEnabled()){
+            Log.d(TAG, "enableDisableBT: enabling BT.");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver, BTIntent);
+
+        }
+        else if(mBluetoothAdapter.isEnabled()){
+            Log.d(TAG, "enableDisableBT: disabling BT.");
+            mBluetoothAdapter.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver, BTIntent);
+
+        }
+
+
+
+    }
+
+
+
 
     public void setActionBarTitle(String title) {
         getSupportActionBar().setTitle(title);
--- Robo_Robi/app/src/main/java/com/example/robo_robi/ui/home/HomeFragment.java
+++ Robo_Robi/app/src/main/java/com/example/robo_robi/ui/home/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.robo_robi.ui.home;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -22,13 +25,19 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    int tries = 0;
+    int maxTries = 3;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         onResume("Home");
+
+        final MainActivity activity = new MainActivity();
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView connTextView = root.findViewById(R.id.Connection);
+        final TextView connTextView = (TextView) root.findViewById(R.id.Connection);
         connTextView.setText(homeViewModel.setConnText().getValue());
         homeViewModel.getConnText().observe(this, new Observer<String>() {
             @Override
@@ -36,7 +45,7 @@ public class HomeFragment extends Fragment {
                 connTextView.setText(s);
             }
         });
-        final TextView welcomeTextView = root.findViewById(R.id.Welcome_Message);
+        final TextView welcomeTextView = (TextView) root.findViewById(R.id.Welcome_Message);
         welcomeTextView.setText(homeViewModel.setWelcomeText().getValue());
         homeViewModel.getWelcomeText().observe(this, new Observer<String>() {
             @Override
@@ -47,13 +56,20 @@ public class HomeFragment extends Fragment {
 
         final Button btnClick;
 
-        btnClick = root.findViewById(R.id.testingButton);
+        btnClick = (Button) root.findViewById(R.id.testingButton);
 
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                updateLiveData();
+                activity.enableDisableBT();
+
+                if (activity.mBluetoothAdapter.isEnabled()){
+                    updateLiveData(true);
+                }
+                else {
+                    updateLiveData(false);
+                }
 
             }
         });
@@ -64,16 +80,13 @@ public class HomeFragment extends Fragment {
     }
 
     // updates livedata from homeViewModel
-    public void updateLiveData(){
+    private void updateLiveData(boolean trueFalse){
 
-        Boolean value =  homeViewModel
-                .getBool()
-                .getValue();
+        homeViewModel.SetBool(trueFalse);
 
-
-
-        if(value == true) {
-                homeViewModel
+        /*if(value == true) {
+            if (activity.bt_adapter.isEnabled())
+            homeViewModel
                         .SetBool(false);
         }
 
@@ -81,7 +94,7 @@ public class HomeFragment extends Fragment {
             homeViewModel
                     .SetBool(true);
 
-        }
+        }*/
 
         homeViewModel.setConnText();
         homeViewModel.setWelcomeText();
