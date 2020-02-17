168eac0c426259ea913b54bb473193bcb861f6c2
Leo Jensen <lion0005@zbc.dk>
2020-02-17 15:48:54 +0100

    Daily Commit

---
+++ RobiControl/app/src/main/java/com/example/robicontrol/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.robicontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View homeView;
+
+    }
+
+
+
+}
