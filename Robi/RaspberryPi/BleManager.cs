b21a1c75b56d41cc2970523986011855812a6a28
Martin Rosenquist <mrquist_94@hotmail.com>
2020-03-25 16:06:55 +0100

    Bluetooth LE
    Implemented Bluetooth LE advertisement watcher

---
+++ Robi/RaspberryPi/BleManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+
+namespace RaspberryPi
+{
+    class BleManager
+    {
+        BluetoothLEAdvertisementWatcher watcher = new BluetoothLEAdvertisementWatcher();
+
+        public void StartWatchingAdvertisement()
+        {
+            BluetoothLEManufacturerData manufacturerData = new BluetoothLEManufacturerData();
+            manufacturerData.CompanyId = 0xFFFF;
+
+            watcher.AdvertisementFilter.Advertisement.ManufacturerData.Add(manufacturerData);
+
+            watcher.Received += Watcher_Received;
+            watcher.Start();
+        }
+
+        public void StopWatchingAdvertisement()
+        {
+            watcher.Received -= Watcher_Received;
+            watcher.Stop();
+        }
+
+        private void Watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            if ((args.Advertisement.ManufacturerData.Count >= 1))
+            {
+                using (var dataReader = DataReader.FromBuffer(args.Advertisement.ManufacturerData[0].Data))
+                {
+                    dataReader.UnicodeEncoding = UnicodeEncoding.Utf8;
+                    Debug.WriteLine($"Advertisement received {args.Timestamp} Data: {dataReader.ReadString(args.Advertisement.ManufacturerData[0].Data.Length)}");
+                }
+            }
+        }
+    }
+}
